62635816a5521e7f64e9c17bccd8e77724e06896
Lee-JuHwan <https://github.com/Lee-JuHwan/gis4_2.git>
2021-07-27 14:44:23 +0900

    07/27 commit 1

--- profileapp/views.py
+++ profileapp/views.py
@@ -5,9 +5,10 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from profileapp.forms import ProfileCreationForm
-
+from profileapp.models import Profile
 
 class ProfileCreateView(CreateView):
-    model = ProfileCreateViewform_class = ProfileCreationForm
+    model = Profile
+    form_class = ProfileCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
-    template_name = 'profileapp/creat.html'
+    template_name = 'profileapp/create.html'
