8a5735c5d729ee113fbe73898c4da5646a8d3f37
Lee-JuHwan <ljk8190@naver.com>
2021-07-28 17:16:33 +0900

    07/28 commit 2-3

--- profileapp/views.py
+++ profileapp/views.py
@@ -16,3 +16,11 @@ class ProfileCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class ProfileUpdateView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    context_object_name = 'target_profile'
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
