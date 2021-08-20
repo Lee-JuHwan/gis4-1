06c8403462323bd1a463d4ebc9df25da202e4230
Lee-JuHwan <ljk8190@naver.com>
2021-08-20 18:24:25 +0900

    8/19 commit 4 - subscribeapp - subscriptionListView(subscriptionlist Template & View) + hearder

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -5,8 +5,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
@@ -28,3 +29,19 @@ class SubscriptionView(RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
         return reverse('projectapp:detail', kwargs={'pk': kwargs['project_pk']})
+
+
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+    paginate_by = 20
+
+    # 기존 --.objects.filter(user=--, project=--) 는 filter 내부가 and 조건으로 구성됨
+    # 따라서 filter 내부의 모든 조건을 만족해야하므로 or 조건을 사용하기 위해선 다른 방법이 필요함
+    # 여기에 --.objects.filter([모델]__in = [ ]) 문법을 통해 or 기능을 사용할 수 있음
+
+    def get_queryset(self):
+        project_list = Subscription.objects.filter(user=self.request.user).values_list('project')
+        article_list = Article.objects.filter(project__in=project_list)
+        return article_list
